dafdedfba645daacddadcb07cca2f8af5ba60e18
whs31 <75864292+whs31@users.noreply.github.com>
2023-06-21 23:15:53 +0300

    rm

--- examples.js
+++
@@ -1,9 +0,0 @@
-var examples =
-[
-    [ "connect", "de/d87/a00456.html", null ],
-    [ "startTelemetrySocket", "da/d65/a00458.html", null ],
-    [ "executeCommand", "db/db1/a00460.html", null ],
-    [ "argument", "de/dcb/a00462.html", null ],
-    [ "setArgument", "d2/d9d/a00464.html", null ],
-    [ "some_script", "db/d2c/a00466.html", null ]
-];
--- namespaces_dup.js
+++
@@ -1,7 +0,0 @@
-var namespaces_dup =
-[
-    [ "Config", "dd/df3/a00219.html", "dd/df3/a00219" ],
-    [ "Map", "dc/d28/a00223.html", "dc/d28/a00223" ],
-    [ "Namespace", "d5/d0b/a00220.html", null ],
-    [ "Processing", "dc/d55/a00226.html", "dc/d55/a00226" ]
-];
